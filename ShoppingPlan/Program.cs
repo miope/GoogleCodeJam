2b3db945cb1085a6e286e7d78f98d02fbd843333
unknown <mperunicic@.global.sdl.corp>
2011-09-19 10:59:40 +0200

    - implemented the dynamic programmng algorithm for ShoppingPlan (without   perishable projects constraint)

--- ShoppingPlan/Program.cs
+++ ShoppingPlan/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
+using System.Globalization;
 
 namespace ShoppingPlan
 {
@@ -13,45 +14,45 @@ namespace ShoppingPlan
             string _inputFile = args[0];
             string _outputFile = Path.ChangeExtension(_inputFile, "out");
 
-            using (var _file = new StreamReader(_inputFile))
+            using (var _input = new StreamReader(_inputFile))
             {
-                var _home = new Location("home", 0, 0);
-                var _testCases = _file.ReadLine();
-                string _line;
-                int _case = 0;
+                using (var _output = new StreamWriter(_outputFile))
+                { 
+                    var _home = new Location("home", 0, 0);
+                    var _testCases = _input.ReadLine();
+                    string _line;
+                    int _case = 0;
 
-                while((_line = _file.ReadLine()) != null)
-                {
-                    _case++;
-                    var _parts = _line.Split(' ');
-                    var _numOfProducts = int.Parse(_parts[0]);
-                    var _numOfShops = int.Parse(_parts[1]);
-                    var _gasPrice = double.Parse(_parts[2]);
+                    while((_line = _input.ReadLine()) != null)
+                    {
+                        _case++;
 
-                    IEnumerable<Product> _products = _file.ReadLine().Split(' ').Select(s => new Product(s));
-                    var _shops = new List<Shop>();
-                    Enumerable.Range(1, _numOfShops).ToList().ForEach(x => {
-                        _parts = _file.ReadLine().Split(' ');
-                        var _aShop = new Shop(x.ToString(), int.Parse(_parts[0]), int.Parse(_parts[1]));
-                        _aShop.Products = _parts.Skip(2).Select(s => {
-                            var _a = s.Split(':');
-                            return new Product(_a[0], double.Parse(_a[1]));
+                        var _parts = _line.Split(' ');
+                        var _numOfProducts = int.Parse(_parts[0]);
+                        var _numOfShops = int.Parse(_parts[1]);
+                        var _gasPrice = double.Parse(_parts[2]);
+
+                        IEnumerable<Product> _products = _input.ReadLine().Split(' ').Select(s => new Product(s));
+
+                        var _shops = new List<Shop>();
+                        Enumerable.Range(1, _numOfShops).ToList().ForEach(x => {
+                            _parts = _input.ReadLine().Split(' ');
+                            var _aShop = new Shop(x.ToString(), int.Parse(_parts[0]), int.Parse(_parts[1]));
+                            _aShop.Products = _parts.Skip(2).Select(s => {
+                                var _a = s.Split(':');
+                                return _products.GetByName(_a[0]).Clone(double.Parse(_a[1]));
+                            });
+                            _shops.Add(_aShop);
                         });
-                        _shops.Add(_aShop);
-                    });
 
-                    _shops.SelectMany(s => s.PossiblePurchases(_home, _gasPrice))
-                                            .Take(5)
-                                            .ToList()
-                                            .ForEach(p => {
-                                                Console.WriteLine(p.Shop.Name);
-                                                Console.WriteLine(string.Format("{0} - {1}", p.Product.Name, p.Cost));
-                                                Console.WriteLine();
-                                            });
+                        var _shoppingPlan = _products.GetShoppingPlan(_shops, _home, _gasPrice);
+                        var _minAmount = _shoppingPlan.Sum(p => p.Cost) + _shoppingPlan.Last().Shop.DistanceTo(_home) * _gasPrice;
+
+                        _output.WriteLine(string.Format("Case #{0}: {1}", _case, Math.Round(_minAmount, 7).ToString("F7", new CultureInfo("en-US"))));
+
+                    }
                 }
             }
-
-            Console.ReadLine();
         }
     }
 
@@ -66,6 +67,14 @@ namespace ShoppingPlan
             Name = name;
             X = x;
             Y = y;
+        }
+
+        public bool IsHome
+        {
+            get
+            {
+                return this.X == 0 && this.Y == 0;
+            }
         }
 
         public double DistanceTo(Location otherLocation)
@@ -100,10 +109,7 @@ namespace ShoppingPlan
 
         public bool IsPerishable
         {
-            get
-            {
-                return Name.EndsWith("!");
-            }
+            get { return Name.EndsWith("!"); }
         }
 
         public Product(string name)
@@ -127,14 +133,9 @@ namespace ShoppingPlan
 
     public static class Extensions
     {
-        public static double Squared(this double num)
-        {
-            return Math.Pow(num, 2d);
-        }
-
         public static double Squared(this int num)
         {
-            return ((double)num).Squared();
+            return Math.Pow((double)num, 2.0);
         }
 
         public static Product GetByName(this IEnumerable<Product> products, string name)
@@ -145,6 +146,29 @@ namespace ShoppingPlan
         public static Product Clone(this Product product, double newPrice)
         {
             return new Product(product.Name, newPrice);
+        }
+
+        public static IEnumerable<Purchase> GetShoppingPlan(this IEnumerable<Product> products, IEnumerable<Shop> shops, 
+            Location home, double gasPrice)
+        {
+
+            var _purchases = new List<Purchase>();
+            var _products = new List<Product>(products);
+            var _destination = home;
+
+            while (_products.Any())
+            {
+                var _purchase = shops.SelectMany(s => s.PossiblePurchases(_destination, gasPrice))
+                                     .Where(p => _products.Select(o => o.Name).Contains(p.Product.Name))
+                                     .OrderBy(p => p.Cost)
+                                     .First();
+
+                _purchases.Add(_purchase);
+                _destination = _purchase.Shop;
+                _products.Remove(_products.GetByName(_purchase.Product.Name));
+            }
+
+            return _purchases;
         }
     }
 }
